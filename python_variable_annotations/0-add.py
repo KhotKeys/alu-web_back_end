2dda42c3539925580cb40fb525216bac5f96b3b5
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2025-05-08 23:10:31 -0700

    first commit

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    return a + b
+
+# Call the function and print the result
+if __name__ == "__main__":
+    result = add(3.5, 2.5)
+    print("The sum is:", result)
+
